dfc0a3bf41175edc79ef85884692a2c11c4e4e0a
Songming Fan <raydhdhhd@gmail.com>
2023-12-14 13:54:36 -0500

    slides controller layout test

--- app/src/main/java/ch/virt/smartphonemouse/ui/SlidesControllerFragment.kt
+++ app/src/main/java/ch/virt/smartphonemouse/ui/SlidesControllerFragment.kt
@@ -65,7 +65,7 @@ class SlidesControllerFragment: Fragment(R.layout.fragment_slides_controller) {
      * Reads the settings for the fragment from the preferences.
      */
     private fun readSettings() {
-        val prefs = PreferenceManager.getDefaultSharedPreferences(context!!)
+        val prefs = PreferenceManager.getDefaultSharedPreferences(requireContext())
         theme = prefs.getString("interfaceTheme", "dark") == "dark"
         scrollThreshold = prefs.getInt("interfaceBehaviourScrollStep", 50)
         middleClickWait = prefs.getInt("interfaceBehaviourSpecialWait", 300)
