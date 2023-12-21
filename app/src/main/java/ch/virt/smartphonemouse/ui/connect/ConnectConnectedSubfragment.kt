8616bf3a13a396544b67601707ac53cfcf372e27
hzy <1543012653@qq.com>
2023-12-21 13:00:43 -0500

    connect connected

--- app/src/main/java/ch/virt/smartphonemouse/ui/connect/ConnectConnectedSubfragment.kt
+++ app/src/main/java/ch/virt/smartphonemouse/ui/connect/ConnectConnectedSubfragment.kt
@@ -1,42 +1,35 @@
 package ch.virt.smartphonemouse.ui.connect
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Chronometer
 import android.widget.TextView
-import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import ch.virt.smartphonemouse.MainActivity
 import ch.virt.smartphonemouse.R
 import ch.virt.smartphonemouse.transmission.BluetoothHandler
 
-/**
- * This class is a sub fragment for the connect page.
- * This fragment is displayed when the app has a successful connection.
- */
+// This class is a sub fragment for the connect page.
 private const val TAG = "ConnectedFragment"
-class ConnectConnectedSubfragment
-/**
- * Creates this sub fragment.
- *
- * @param bluetooth bluetooth handler to read status from
- */(private val bluetooth: BluetoothHandler?) : Fragment(R.layout.subfragment_connect_connected) {
+class ConnectConnectedSubfragment (private val bluetooth: BluetoothHandler?) :
+    Fragment(R.layout.subfragment_connect_connected) {
     private var elapsed: Chronometer? = null
     private var name: TextView? = null
     private var disconnect: Button? = null
     private var mouse: Button? = null
+    private var touchpad: Button? = null
+    private var slide: Button? = null
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         elapsed = view.findViewById(R.id.connect_connected_device_elapsed)
         name = view.findViewById(R.id.connect_connected_device_name)
         disconnect = view.findViewById(R.id.connect_connected_disconnect)
         mouse = view.findViewById(R.id.connect_connected_mouse)
+        touchpad = view.findViewById(R.id.connect_connected_touchpad)
+        slide = view.findViewById(R.id.connect_connected_slide)
         if (bluetooth!!.isConnected) {
-            name?.setText(bluetooth.host?.device?.name)
+            name?.setText(bluetooth.host!!.device!!.name)
             bluetooth.host?.let { elapsed?.setBase(it.connectedSince) }
             elapsed?.setFormat(resources.getString(R.string.connect_connected_elapsed))
             elapsed?.start()
@@ -46,13 +39,16 @@ class ConnectConnectedSubfragment
                     R.id.drawer_mouse
                 )
             })
+            touchpad?.setOnClickListener(View.OnClickListener { v: View? ->
+                (activity as MainActivity?)!!.navigate(
+                    R.id.drawer_touchpad
+                )
+            })
+            slide?.setOnClickListener(View.OnClickListener { v: View? ->
+                (activity as MainActivity?)!!.navigate(
+                    R.id.drawer_slides_controller
+                )
+            })
         }
     }
-
-//    override fun onStop() {
-//        super.onStop()
-//        Log.d(TAG, "onstop()")
-////        bluetooth?.host?.disconnect()
-//    }
-
 }
